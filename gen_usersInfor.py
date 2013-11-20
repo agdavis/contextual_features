1203034de6b3bbb8e4c8cc08d5f6fe30d951d0e3
Alexandre Guelman <agdavis@cerberus.lvad.speed.dcc.ufmg.br>
2013-11-19 22:32:42 -0200

    - Bugs

--- gen_usersInfor.py
+++ gen_usersInfor.py
@@ -3,6 +3,7 @@ from Tweetstream import *
 import sys
 
 if __name__ == "__main__":
-	Tweetstream ts(sys.argv[2])
-	UserAnalyser ua(sys.argv[1], tweetstream=ts)
-	ua.compute_userVectors()
+	keywords = open(sys.argv[3], 'r').readline().strip("\n").split(",")
+	ts = Tweetstream (jsonfilee=sys.argv[2], jsonformat=False, keywords=keywords)
+	ua = UserAnalyser (sys.argv[1], tweetstream=ts)
+	ua.compute_usersVectors()
