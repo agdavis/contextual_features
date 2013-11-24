fb4c3c802feb60f223988cbba67369df14414a4f
agdavis <alexandre.davis@gmail.com>
2013-11-24 00:52:05 -0200

    Time(user-TopicStream) added

---
+++ gen_time_user_topicstream.py
@@ -0,0 +1,40 @@
+from datetime import *
+from Tweetstream import *
+from UserAnalyser import *
+from TimeAnalyser import *
+import math
+import sys
+import pickle
+#Frequency over the common
+
+
+if __name__ == "__main__":
+	keywords = open(sys.argv[2], 'r').readline().strip("\n").split(",")
+	userstream = Tweetstream(jsonfilee=sys.argv[3], jsonformat=False, keywords=keywords)
+	topicstream = Tweetstream(jsonfilee=sys.argv[1], jsonformat=False, keywords=keywords)
+	ua = UserAnalyser (sys.argv[4], keywords = keywords)
+	ua.load_usersVectors()
+	ua.load_idf()
+	ua.load_usersScore()
+	tw = TimeAnalyser(userstream=userstream, topicstream=topicstream, userinfo=ua)
+	nwindow = 0
+	rank = dict()
+	for (userwindow, topicwindow) in tw:
+		score = 0.0
+		users = set()
+		
+		temp = True	
+		for t in topicwindow:
+			if temp:
+				temp = False
+				print t['created_at'] + '\t',
+			users.add(t['user_id'])
+
+		for user in users:
+			score += ua.usersScore[user]
+		score /= len(users)
+		print score
+		for t in userwindow:
+			rank[t['id']] = score
+		#prinit score, nwindow
+	pickle.dump(rank, open(sys.argv[4]+"_rank_users_tstream.pick", 'w'), pickle.HIGHEST_PRIORITY)
