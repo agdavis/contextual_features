b581ec7209ace41c2972ff1b559e3ba7b00bcf56
agdavis <alexandre.davis@gmail.com>
2013-12-05 16:35:40 -0200

    2 User features

---
+++ gen_user_tsNorm.py
@@ -0,0 +1,34 @@
+from datetime import *
+from Tweetstream import *
+from UserAnalyser import *
+from TimeAnalyser import *
+import math
+import sys
+import pickle
+#Frequency over the common
+
+
+if __name__ == "__main__":
+	keywords = open(sys.argv[2], 'r').readline().strip("\n").split(",")
+	userstream = Tweetstream(jsonfilee=sys.argv[3], jsonformat=False, keywords=keywords)
+	topicstream = Tweetstream(jsonfilee=sys.argv[1], jsonformat=False, keywords=keywords)
+	ua = UserAnalyser (sys.argv[4], keywords = keywords)
+	ua.load_usersVectors()
+	ua.load_idf()
+	ua.load_usersScore()
+	rank = dict()
+	tw = TimeAnalyser(userstream=userstream, topicstream=topicstream, userinfo=ua)
+	nwindow = 0
+# normalizar pelo numero de kw no topic vector
+
+	for (userwindow, topicwindow) in tw:
+		users = set()
+		for t in topicwindow:
+			users.add(t['user_id'])
+
+		for t in userwindow:
+			score = 0.0
+			if t['user_id'] in ua.usersScore:
+				rank[t['id']] = ua.usersScore[t['user_id']]/log(len(users))
+			#prinit score, nwindow
+	pickle.dump(rank, open(sys.argv[4]+"_rank_USER_tsNorm.pick", 'w'), pickle.HIGHEST_PROTOCOL)
---
+++ gen_user_usNorm.py
@@ -0,0 +1,32 @@
+from datetime import *
+from Tweetstream import *
+from UserAnalyser import *
+from TimeAnalyser import *
+import math
+import sys
+import pickle
+#Frequency over the common
+
+
+if __name__ == "__main__":
+	keywords = open(sys.argv[2], 'r').readline().strip("\n").split(",")
+	userstream = Tweetstream(jsonfilee=sys.argv[3], jsonformat=False, keywords=keywords)
+	topicstream = Tweetstream(jsonfilee=sys.argv[1], jsonformat=False, keywords=keywords)
+	ua = UserAnalyser (sys.argv[4], keywords = keywords)
+	ua.load_usersVectors()
+	ua.load_idf()
+	ua.load_usersScore()
+	rank = dict()
+	
+	ntweets = dict()
+	for t in userstream:
+		if not(t['user_id'] in ntweets): t['user_id'] = 1
+		else: t['user_id'] += 1
+
+	# normalizar pelo numero de kw no topic vector
+	for t in userstream:
+		score = 0.0
+		if t['user_id'] in ua.usersScore:
+			rank[t['id']] = (ua.usersScore[t['user_id']]/ntweets[t['user_id']])
+		#prinit score, nwindow
+	pickle.dump(rank, open(sys.argv[4]+"_rank_USER_usNorm.pick", 'w'), pickle.HIGHEST_PROTOCOL)
